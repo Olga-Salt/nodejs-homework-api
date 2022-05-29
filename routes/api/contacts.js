cf0349d39626a6471440f60437ca09ebf7d73f6a
Olga Saltanovskaya <saltanoffskaya@gmail.com>
2022-05-29 14:15:39 +0300

    [feat] Set up routing to work with a collection of contacts.
    *Js*
    - Create hw02-express branch from master branch
    The REST API must support the following routes:
    GET /api/contacts
    GET /api/contacts/:id
    POST /api/contacts
    DELETE /api/contacts/:id
    PUT /api/contacts/:id
    - For routes that accept data (POST and PUT), consider checking (validating) the received data. Use the joi package to validate received data.

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,77 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
+const {
+  addPostPutValidation,
+} = require("../../middlewares/validationMiddleWare");
 
-const router = express.Router()
+const contacts = require("../../models/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const all = await contacts.listContacts();
+    res.json(all);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contactById = await contacts.getContactById(contactId);
+    if (!contactById) {
+      res.status(404).json({ message: "Not found" });
+    } else {
+      res.json(contactById);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", addPostPutValidation, async (req, res, next) => {
+  try {
+    const { name, email, phone } = req.body;
+    const newContact = await contacts.addContact({ name, email, phone });
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
+});
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    console.log(contactId);
+    const removedContact = await contacts.removeContact(contactId);
+    if (!removedContact) {
+      res.status(404).json({ message: "Not Found" });
+    } else {
+      res.status(200).json({ message: "contact deleted" });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", addPostPutValidation, async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const { name, email, phone } = req.body;
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    const updatedContact = await contacts.updateContact(contactId, {
+      name,
+      email,
+      phone,
+    });
 
-module.exports = router
+    if (!updatedContact) {
+      res.status(404).json({ message: "Not Found" });
+    } else {
+      res.json(updatedContact);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
